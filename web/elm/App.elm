e3f522d50dd62d9483e8585e71b66b6664ddb11f
Julian Haeger <julianhaeger@gmail.com>
2016-03-14 08:16:29 +0000

    can add items with custom text

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -12,7 +12,7 @@ type alias Model = {
   items : List Item,
   nextuid : Int,
   bottomRight : Position,
-  placeholder : Maybe Position
+  placeholder : Maybe Item
 }
 
 type alias Item = {
@@ -44,46 +44,108 @@ emptyModel =
 type Action
   = NoOp
   | ShowPlaceholder Position
+  | UpdateDefinition String
+  | Add String
 
 update : Action -> Model -> Model
 update action model =
   case action of
     NoOp -> model
-    ShowPlaceholder position -> {model | placeholder = Just position}
 
+    ShowPlaceholder position -> {model | placeholder = Just (Item "placeholder" position 0)}
+
+    UpdateDefinition definition ->
+      {model | placeholder = (updatePlaceholderDefinition model.placeholder definition)}
+
+    Add definition ->
+      let item = {
+        definition = definition,
+        position = (getPositionFromPlaceholder model.placeholder),
+        uid = model.nextuid
+      }
+      in
+        { model |
+          placeholder = Nothing,
+          items = model.items ++ [item],
+          nextuid = model.nextuid + 1
+        }
+
+
+getPositionFromPlaceholder : Maybe Item -> Position
+getPositionFromPlaceholder maybe =
+  case maybe of
+    Nothing -> Position 0 0
+    Just placeholder -> placeholder.position
+
+
+updatePlaceholderDefinition : Maybe Item -> String -> Maybe Item
+updatePlaceholderDefinition maybe newDefinition =
+  case maybe of
+    Nothing -> Nothing
+    Just item -> Just {item | definition = newDefinition}
 
 ------ View ------
-
 view: Address Action -> Model -> Html
 view address model =
+  div
+    []
+    [
+      (boardView address model),
+      inputView address model
+    ]
+
+inputView: Address Action -> Model -> Html
+inputView address model =
+  div
+    [
+      style [("width", "100%"), ("position","fixed"), ("bottom", "0")]
+    ]
+    [
+      input
+        [
+          style [("width", "100%") ],
+          (placeholder "type..."),
+          (autofocus True),
+          on "change" targetValue (Signal.message address << Add)
+        ]
+        [
+        ]
+    ]
+
+boardView: Address Action -> Model -> Html
+boardView address model =
   div
     [
       class "board",
       style [
         ("position", "relative"),
-        ("background-color","whitesmoke"),
         ("width", toString model.bottomRight.x ++ "px"),
         ("height", toString model.bottomRight.y ++ "px")
       ],
       on "click" eventPos (Signal.message address << ShowPlaceholder)
     ]
+    ((List.map itemView model.items) ++ [placeholderView model.placeholder])
+
+
+itemView : Item -> Html
+itemView item =
+  div
     [
-      placeholderView model.placeholder
+      class "placeholder item",
+      style [
+        ("position", "absolute"),
+        ("background-color","whitesmoke"),
+        ("top", toPxString item.position.y),
+        ("left", toPxString item.position.x)]
+    ]
+    [
+      text item.definition
     ]
 
-placeholderView: Maybe Position -> Html
+placeholderView: Maybe Item -> Html
 placeholderView maybe =
   case maybe of
-    Just position ->
-      div
-        [
-          class "placeholder item",
-          style [("position", "absolute"), ("top", toPxString position.y), ("left", toPxString position.x)]
-        ]
-        [
-          text "placeholder"
-        ]
-
+    Just item -> itemView item
     Nothing -> text "click"
 
 toPxString: a -> String
